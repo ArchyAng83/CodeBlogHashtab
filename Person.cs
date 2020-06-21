334a34b7166738e883f95afbb76f9310b6ddc1c9
saltkeeper1@gmail.com <saltkeeper1@gmail.com>
2020-06-21 22:33:09 +0300

    Изменен HashTable в Programm

--- Person.cs
+++ Person.cs
@@ -5,6 +5,7 @@ namespace CodeBlogHashtab
 {
     class Person
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public int Age { get; set; }
         public int Gender { get; set; }
--- Program.cs
+++ Program.cs
@@ -23,14 +23,17 @@ namespace CodeBlogHashtab
 
             Console.ReadLine();
 
-            var hashTable = new HashTable<int, string>(100);
-            hashTable.Add(5, "Привет");
-            hashTable.Add(18, "Мир");
-            hashTable.Add(777, "Здравствуй");
-            hashTable.Add(7, "Программист");
+            var person1 = new Person() { Name = "Петя", Age = 35, Gender = 0, Id = 1 };
+            var person2 = new Person() { Name = "Андрей", Age = 23, Gender = 0, Id = 2 };
+            var person3 = new Person() { Name = "Аня", Age = 30, Gender = 1, Id = 3 };
+            var hashTable = new HashTable<int, Person>(100);
+            hashTable.Add(person1.Id, person1);
+            hashTable.Add(person2.Id, person2);
+            hashTable.Add(person3.Id, person3);
+            
 
-            Console.WriteLine(hashTable.Search(6, "Вася"));
-            Console.WriteLine(hashTable.Search(18, "Мир"));
+            Console.WriteLine(hashTable.Search(person1.Id, person1));
+            Console.WriteLine(hashTable.Search(18, person2));
 
             Console.ReadLine();
 
@@ -38,7 +41,7 @@ namespace CodeBlogHashtab
             badHashTable.Add(5);
             badHashTable.Add(18);
             badHashTable.Add(777);
-            badHashTable.Add(6);
+            badHashTable.Add(7);
 
             Console.WriteLine(badHashTable.Search(6));
             Console.WriteLine(badHashTable.Search(18));
